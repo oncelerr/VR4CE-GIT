a84c2cd55fb219833a1bd791a2f2587a27afe097
oncelerr <markjonathan368@gmail.com>
2024-05-08 01:18:13 +0800

    new

--- Assets/Scripts/triggerZoneGloves.cs
+++ Assets/Scripts/triggerZoneGloves.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Playables;
 
 public class triggerZoneGloves : MonoBehaviour
 {
@@ -9,6 +11,9 @@ public class triggerZoneGloves : MonoBehaviour
     public UnityEvent<GameObject> OnEnterEvent;
     public Material newMaterial; // Reference to the new material to be applied
     private bool done = false;
+    public AudioClip audioClip;
+    public AudioClip audioClip1;
+    public PlayableDirector timeline;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -16,7 +21,16 @@ public class triggerZoneGloves : MonoBehaviour
         {
             OnEnterEvent.Invoke(other.gameObject);
             done = true;
-            
+        } else
+        {
+            if (!other.gameObject.CompareTag("Untagged") && !other.gameObject.CompareTag("Human") && timeline.state == PlayState.Paused && other.gameObject.CompareTag("gloveLeft"))
+            {
+                AudioSource.PlayClipAtPoint(audioClip1, new Vector3(9.36299992f, 1.10899997f, -0.841000021f));
+            }
+            else if (!other.gameObject.CompareTag("Untagged") && !other.gameObject.CompareTag("Human") && timeline.state == PlayState.Paused)
+            {
+                AudioSource.PlayClipAtPoint(audioClip, new Vector3(9.36299992f, 1.10899997f, -0.841000021f));
+            } 
         }
     }
 
